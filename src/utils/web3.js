231a00026131fd9649b5211584a42dd77e227a9d
Moneyard-Frontend <moneyard2@gmail.com>
2025-04-03 00:03:53 +0200

    🚧 temp: Add placeholder Infura key and contract address

---
+++ src/utils/web3.js
@@ -0,0 +1,6 @@
+// TEMPORARY PLACEHOLDERS - REPLACE LATER!
+export const INFURA_KEY = 'test-infura-key';
+export const CONTRACT_ADDRESS = '0xTEST_CONTRACT_ADDRESS';
+
+// Real initialization code here...
+export const web3 = new Web3(`https://mainnet.infura.io/v3/${INFURA_KEY}`);
