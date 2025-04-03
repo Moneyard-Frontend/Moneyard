e47b5c49d0fea9dd59ee40c3145eea3210f3f6ac
Moneyard-Frontend <moneyard2@gmail.com>
2025-04-03 02:03:26 +0200

    Delete src/utils/web3.js

--- src/utils/web3.js
+++
@@ -1,6 +0,0 @@
-// TEMPORARY PLACEHOLDERS - REPLACE LATER!
-export const INFURA_KEY = 'test-infura-key';
-export const CONTRACT_ADDRESS = '0xTEST_CONTRACT_ADDRESS';
-
-// Real initialization code here...
-export const web3 = new Web3(`https://mainnet.infura.io/v3/${INFURA_KEY}`);
